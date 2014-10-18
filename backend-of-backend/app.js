f26301ee43c75a8d945e75498de7577519e1f9be
AlexEKoren <alex.koren@jhu.edu>
2014-10-18 05:39:14 -0700

    Stuff

--- backend-of-backend/app.js
+++ backend-of-backend/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var exec = require('child_process').exec;
 
 var STATE_READY = 0;
 var STATE_CREATING = 1;
@@ -13,17 +14,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(multer({
-  inMemory: true
+  dest: './uploads/'
 }));
 
 app.post('/create', function(req, res) {
-  console.log(req.files);
   if (state != STATE_READY) {
     console.log("something terrible happened");
+    res.send("failure");
+    return;
   }
-  var zip = req.files.z;
-  // make zip into ipa
-  console.log(zip);
+  var zip = req.files.file;
+
+  exec('./compile_ipa.sh '+process.cwd()+'/'+zip.path, {cwd: process.cwd()},
+    function(error, stdout, stderr) {
+    console.log('stdout: ', stdout);
+    console.log('stderr: ', stderr);
+    if (error !== null) {
+      console.log('exec error: ', error);
+    }
+  });
 
   res.send(zip);
 });
